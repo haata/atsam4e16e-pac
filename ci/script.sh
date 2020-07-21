e04a5de62102677bbdd43cd74f9278cb41054b24
John W. Terrell <john@coolpeoplenetworks.com>
2020-07-21 14:11:02 -0700

    Removed generation via rust2svd from CI script.   I'ts unnecessary.

--- ci/script.sh
+++ ci/script.sh
@@ -5,8 +5,6 @@ main() {
     cargo check --target $TARGET --features rt
 
     if [ $TARGET = x86_64-unknown-linux-gnu ]; then
-        # Generate the PAC
-        bash update.sh
         cargo clean
         cargo build
     fi
