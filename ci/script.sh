c7c38db577619c3ff2604a35204133b134388b08
John W. Terrell <john@coolpeoplenetworks.com>
2020-07-21 11:16:28 -0700

    Added initial travis-ci scripts

---
+++ ci/script.sh
@@ -0,0 +1,15 @@
+set -ex
+
+main() {
+    cargo check --target $TARGET
+    cargo check --target $TARGET --features rt
+
+    if [ $TARGET = x86_64-unknown-linux-gnu ]; then
+        # Generate the PAC
+        bash ../update.sh
+        cargo clean
+        cargo build
+    fi
+}
+
+main
