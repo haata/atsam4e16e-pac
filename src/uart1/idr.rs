fb379b10c019a07d98eb07f2c7f829a8b4f31454
John W. Terrell <john@coolpeoplenetworks.com>
2020-09-26 15:10:00 -0700

    Updated build from latest cleaned SVD.

--- src/uart1/idr.rs
+++ src/uart1/idr.rs
@@ -221,42 +221,42 @@ impl<'a> RXBUFF_W<'a> {
     }
 }
 impl W {
-    #[doc = "Bit 0 - Disable RXRDY Interrupt"]
+    #[doc = "Bit 0 - RXRDY Interrupt Disable"]
     #[inline(always)]
     pub fn rxrdy(&mut self) -> RXRDY_W {
         RXRDY_W { w: self }
     }
-    #[doc = "Bit 1 - Disable TXRDY Interrupt"]
+    #[doc = "Bit 1 - TXRDY Interrupt Disable"]
     #[inline(always)]
     pub fn txrdy(&mut self) -> TXRDY_W {
         TXRDY_W { w: self }
     }
-    #[doc = "Bit 3 - Disable End of Receive Transfer Interrupt"]
+    #[doc = "Bit 3 - End of Receive Buffer Transfer Interrupt Disable (available in all USART modes of operation)"]
     #[inline(always)]
     pub fn endrx(&mut self) -> ENDRX_W {
         ENDRX_W { w: self }
     }
-    #[doc = "Bit 4 - Disable End of Transmit Interrupt"]
+    #[doc = "Bit 4 - End of Transmit Buffer Interrupt Disable (available in all USART modes of operation)"]
     #[inline(always)]
     pub fn endtx(&mut self) -> ENDTX_W {
         ENDTX_W { w: self }
     }
-    #[doc = "Bit 5 - Disable Overrun Error Interrupt"]
+    #[doc = "Bit 5 - Overrun Error Interrupt Enable"]
     #[inline(always)]
     pub fn ovre(&mut self) -> OVRE_W {
         OVRE_W { w: self }
     }
-    #[doc = "Bit 6 - Disable Framing Error Interrupt"]
+    #[doc = "Bit 6 - Framing Error Interrupt Disable"]
     #[inline(always)]
     pub fn frame(&mut self) -> FRAME_W {
         FRAME_W { w: self }
     }
-    #[doc = "Bit 7 - Disable Parity Error Interrupt"]
+    #[doc = "Bit 7 - Parity Error Interrupt Disable"]
     #[inline(always)]
     pub fn pare(&mut self) -> PARE_W {
         PARE_W { w: self }
     }
-    #[doc = "Bit 9 - Disable TXEMPTY Interrupt"]
+    #[doc = "Bit 9 - TXEMPTY Interrupt Disable"]
     #[inline(always)]
     pub fn txempty(&mut self) -> TXEMPTY_W {
         TXEMPTY_W { w: self }
@@ -266,7 +266,7 @@ impl W {
     pub fn txbufe(&mut self) -> TXBUFE_W {
         TXBUFE_W { w: self }
     }
-    #[doc = "Bit 12 - Disable Buffer Full Interrupt"]
+    #[doc = "Bit 12 - Receive Buffer Full Interrupt Disable"]
     #[inline(always)]
     pub fn rxbuff(&mut self) -> RXBUFF_W {
         RXBUFF_W { w: self }
